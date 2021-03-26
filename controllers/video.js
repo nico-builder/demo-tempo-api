d741f6b64b55ded5cbf79e76f30a95843d50fd09
Nicolas Farolfi <nicolasfarolfi@MacBook-Air-de-Nicolas.local>
2021-03-26 22:22:01 +0400

    download new file

--- controllers/video.js
+++ controllers/video.js
@@ -1,4 +1,6 @@
 const { exec } = require("child_process");
+const fs = require('fs');
+
 
 exports.resize = (req, res, next) => {
 	const input_video = req.files[0];
@@ -6,11 +8,12 @@ exports.resize = (req, res, next) => {
 
 	console.log("////////// INPUT DATA");
 	console.log("RESIZE OPTION: " + resize_option);
-	console.log("INPUT FILE: " + input_video);
+	console.log("INPUT FILE: " + JSON.stringify(input_video));
 
-	// changer C++ commande ici
-	// exec("COMMAND",
-	exec("ls -la", (error, stdout, stderr) => {
+	// C++ COMMAND TO LAUNCH AUTO FLIP
+	exec("GLOG_logtostderr=1 controllers/mediapipe/bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip \
+	--calculator_graph_config_file=./controllers/mediapipe/mediapipe/examples/desktop/autoflip/autoflip_graph.pbtxt \
+	--input_side_packets=input_video_path="+ input_video.path +",output_video_path=./videos/nouveau.mp4,aspect_ratio=" + resize_option, (error, stdout, stderr) => {
 	    if (error) {
 	    	res.status(500).json(error.message);
 	        console.log(`error: ${error.message}`);
@@ -22,28 +25,13 @@ exports.resize = (req, res, next) => {
 	        return;
 	    }
 	    // SUCCESS
-	    res.status(200).json("OK");
+	    // return video path
+	    res.status(200).json({path: "videos/nouveau.mp4"});
 	    // console.log(`stdout: ${stdout}`);
 	});
 };
 
-
-exports.testResize = (req, res, next) => {
-	exec("GLOG_logtostderr=1 controllers/mediapipe/bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip \
-	--calculator_graph_config_file=./controllers/mediapipe/mediapipe/examples/desktop/autoflip/autoflip_graph.pbtxt \
-	--input_side_packets=input_video_path=./videos/videotest.mp4,output_video_path=./videos/nouveau.mp4,aspect_ratio=1:1", (error, stdout, stderr) => {
-	    if (error) {
-	    	res.status(500).json(error.message);
-	        console.log(`error: ${error.message}`);
-	        return;
-	    }
-	    if (stderr) {
-	    	res.status(500).json(stderr);
-	        console.log(`stderr: ${stderr}`);
-	        return;
-	    }
-	    // SUCCESS
-		console.log(`stdout: ${stdout}`);
-	    res.status(200).json("OK");
-	});
+exports.downloadVideo = (req, res, next) => {
+	const file = req.body.path;
+	res.download(file);
 };
