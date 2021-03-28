725de2db3ee8aa048c44efe3e9d26280197370fa
Nicolas Farolfi <nicolasfarolfi@MacBook-Air-de-Nicolas.local>
2021-03-28 20:42:28 +0400

    cors

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const videoRoutes = require('./routes/video');
 const app = express();
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
 // for parsing application/json
 app.use(bodyParser.json());
