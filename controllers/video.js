1b8928956566cac35f6d6bb2a905a5ff63e6eca3
Nicolas Farolfi <nicolasfarolfi@MacBook-Air-de-Nicolas.local>
2021-03-29 20:33:03 +0400

    push

--- controllers/video.js
+++ controllers/video.js
@@ -17,25 +17,13 @@ exports.resize = (req, res, next) => {
 
 	exec("GLOG_logtostderr=1 controllers/mediapipe/bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip \
 	--calculator_graph_config_file=./controllers/mediapipe/mediapipe/examples/desktop/autoflip/autoflip_graph.pbtxt \
-	--input_side_packets=input_video_path="+ input_video.path +",output_video_path=./videos/nouveau.mp4,aspect_ratio=" + resize_option, (error, stdout, stderr) => {
-	    if (error) {
-	    	console.log(`error: ${error.message}`);
-	    	res.status(500).json(error.message);
-	        // return;
-	    }
-	    if (stderr) {
-	    	console.log(`stderr: ${stderr}`);
-	    	res.status(500).json(stderr);
-	        // return;
-	    }
-	    // SUCCESS
-	    // return video path
-	    if(stdout){
-	    	 console.log("succes, path: videos/nouveau.mp4");
-		    res.status(200).json({path: "videos/nouveau.mp4"});
-		    // console.log(`stdout: ${stdout}`);
-	    }
-	});
+	--input_side_packets=input_video_path="+ input_video.path +",output_video_path=./videos/nouveau.mp4,aspect_ratio=" + resize_option)
+	.then(success => {
+		res.status(200).json({path: "videos/nouveau.mp4"});
+	})
+	.catch(error => {
+		res.status(500).json(error);
+	}); 
 };
 
 exports.downloadVideo = (req, res, next) => {
